5b47d53d8eba1e4da42d93734446c020a8e461b4
Priyanka Negi <negipriyanka2211@gmail.com>
2023-01-15 12:44:18 +0530

    complete

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
 "use strict"
-let boxEl = document.getElementsByClassName("box");
 let statusEl = document.getElementById("status");
-let btnEl = document.getElementById("btn");
-// winning pattern
-let winningPattern = [[0, 1, 2], [3, 4, 5], [6, 7, 8], 
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],  [0, 4, 8], [2, 4, 6]];
 
-let arr = ["", "", "", "", "", "", "", "", ""];
-// winning logic
-let checkWinner = function (){
-    winningPattern.forEach(e=>{
-        if(
-            (arr[0] != null && arr[0] == arr[1] && arr[1] ==arr[2]) ||
-            (arr[3] != null && arr[3] == arr[4] && arr[4] ==arr[5]) ||
-            (arr[6] != null && arr[6] == arr[7] && arr[7] ==arr[8]) ||
-            (arr[0] != null && arr[0] == arr[3] && arr[3] ==arr[6]) ||
-            (arr[1] != null && arr[1] == arr[4] && arr[4] ==arr[7]) ||
-            (arr[2] != null && arr[2] == arr[5] && arr[5] ==arr[8]) ||
-            (arr[0] != null && arr[0] == arr[4] && arr[4] ==arr[8]) ||
-            (arr[2] != null && arr[2] == arr[4] && arr[4] ==arr[6])
-        ){
-            statusEl.innerHTML = turn + "WON!";
-            console.log("Won")
-        }
-    })
+let currentPlayer = "X";
+let arr = Array(9).fill(null);
+
+function checkWinner(){
+    if(
+        (arr[0] != null && arr[0] == arr[1] && arr[1] ==arr[2]) ||
+        (arr[3] != null && arr[3] == arr[4] && arr[4] ==arr[5]) ||
+        (arr[6] != null && arr[6] == arr[7] && arr[7] ==arr[8]) ||
+        (arr[0] != null && arr[0] == arr[3] && arr[3] ==arr[6]) ||
+        (arr[1] != null && arr[1] == arr[4] && arr[4] ==arr[7]) ||
+        (arr[2] != null && arr[2] == arr[5] && arr[5] ==arr[8]) ||
+        (arr[0] != null && arr[0] == arr[4] && arr[4] ==arr[8]) ||
+        (arr[2] != null && arr[2] == arr[4] && arr[4] ==arr[6])
+    ){
+        let mainEl = document.querySelector("main");
+        mainEl.innerText = 
+        `Winner is ${currentPlayer}
+        `
+        return;
+    }
+    if(!arr.some((e) => e== null)){
+        let mainEl = document.querySelector("main");
+        mainEl.innerText =  `"Draw!!"`;
+    }
 }
-// player x plays first
-let turn = "X";
-// change player
-let changePlayer = function(){
-    return turn=="X"?"0":"X"
+
+function handleClick(el){
+    const id = Number(el.id);
+    if (arr[id] !== null) return;
+    arr[id] = currentPlayer;
+    el.innerText = currentPlayer;
+    checkWinner();
+    currentPlayer = currentPlayer === "X" ? "0" : "X"; 
+    statusEl.innerText = `Turn for  ${currentPlayer}`
 }
-// game logic
-Array.from(boxEl).forEach(function(ele)  {
-    ele.addEventListener("click", ()=> {
-        if(ele.innerText ==""){
-            ele.innerText = turn;
-            checkWinner();
-            turn = changePlayer();
-            statusEl.innerHTML = "Turn for " + turn;
-        }
-    })
-});
-// restart game logic
-let reset = function (){
-    Array.from(boxEl).forEach(function(ele){
-        ele.innerText = "";
-    })
-}
+
+
+
+
+
